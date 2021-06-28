99a04322e8dcaa777b2a7414c4d8715ed6f10f47
Dark Tornado <dt3141592@naver.com>
2021-06-28 22:44:05 +0900

    Update response.lua

--- response.lua
+++ response.lua
@@ -1,3 +1,13 @@
+function is_valid_word(word)
+    url = "https://stdict.korean.go.kr/search/searchResult.do?&searchKeyword="..word
+    data = Utils.getWebText(url)
+    if string.find(data, "찾기 결과") then
+        return true;
+    else
+        return false
+    end
+end
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
  
 end
