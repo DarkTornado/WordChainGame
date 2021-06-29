ee04d6bb117bfd9003e4b95112dfa8807aa6aaf1
Dark Tornado <dt3141592@naver.com>
2021-06-29 12:36:17 +0900

    Update response.lua

--- response.lua
+++ response.lua
@@ -2,11 +2,13 @@ function is_valid_word(word)
     url = "https://stdict.korean.go.kr/search/searchResult.do?&searchKeyword="..word
     data = Utils.getWebText(url)
     if string.find(data, "찾기 결과") then
-        return true;
+        data = lsoup(data):select("ul.result"):select("font", 1)
+        return data:text()
     else
-        return false
+        return nul
     end
 end
+
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
  
